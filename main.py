81cd5287760287c474e08f5d5e2c83a3037c1446
czajemi <e.czajaaa@gmail.com>
2021-12-30 17:28:55 +0100

    Piece Class (addition of some logic to game)

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from game import Game
 from board import Board
 
 size = (9, 9)
-board = Board(size)
+prob = 0.5
+board = Board(size, prob)
 screenSize = (800, 800)
 game = Game(board, screenSize)
 game.run()
