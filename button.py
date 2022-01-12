a7b3a39588eba22f0f3cd9ffe6ae296451b4f866
czajemi <e.czajaaa@gmail.com>
2022-01-12 01:13:33 +0100

    adding lambda functions

--- button.py
+++ button.py
@@ -13,6 +13,9 @@ class Button():
         self.fcolour = fcolour
         self.text = text
         self.current = False
+        self.xCor = lambda x, sx, fontSize, text: x + (sx/2) - (fontSize/2)*(len(text)/2) - 5
+        self.yCor = lambda y, sy, fontSize: y + (sy/2) - (fontSize/2) - 4
+        self.add = lambda x, y: x + y
         self.buttonf = pygame.font.SysFont(font, fontSize)
 
     def showButton(self, display):
@@ -21,10 +24,11 @@ class Button():
         else:
             pygame.draw.rect(display, self.bcolour, (self.x, self.y, self.sx, self.sy))
         textSurface = self.buttonf.render(self.text, False, self.fcolour)
-        display.blit(textSurface,((self.x + (self.sx/2) - (self.fontSize/2)*(len(self.text)/2) - 5, (self.y + (self.sy/2) - (self.fontSize/2) -4))))
+        
+        display.blit(textSurface,((self.xCor(self.x, self.sx, self.fontSize, self.text), self.yCor(self.y, self.sy, self.fontSize))))
 
     def focusCheck(self, mousePos, mouseClick):
-        if(mousePos[0] >= self.x and mousePos[0] <= self.x + self.sx and mousePos[1] >= self.y and mousePos[1] <= self.y + self.sy):
+        if(mousePos[0] >= self.x and mousePos[0] <= self.add(self.x, self.sx) and mousePos[1] >= self.y and mousePos[1] <= self.add(self.y, self.sy)):
             self.current = True
             return mouseClick[0]
         else:
